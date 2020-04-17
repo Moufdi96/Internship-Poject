dc03ecdb2a826325461856d7a6f9c51dde8c14dc
MoufdiTaha <moufdi_taha@outlook.fr>
2020-04-17 12:16:21 +0200

    cleaning up the merge mess

--- PepperApp/app/src/main/java/com/example/pepperapp/Controller/HomeFragment.java
+++ PepperApp/app/src/main/java/com/example/pepperapp/Controller/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.pepperapp.Controller;
 
+import android.content.Intent;
+import android.media.Image;
+import android.media.MediaMetadata;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +16,22 @@ import androidx.fragment.app.Fragment;
 import com.example.pepperapp.R;
 
 public class HomeFragment extends Fragment {
+    private ImageView mExercise;
+    private ImageView mActivities;
+    private ImageView mMonitoring;
+    private View mView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment,container,false);
+        mView = inflater.inflate(R.layout.home_fragment,container,false);
+        mExercise = mView.findViewById(R.id.movements);
+        mExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mMvtListActivity = new Intent(getActivity(),MovementCategoryActivity.class);
+                startActivity(mMvtListActivity);
+            }
+        });
+        return mView;
     }
 }
